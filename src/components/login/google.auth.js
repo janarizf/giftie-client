618526c9fd523a83bdbc327c83065f7fcbfb6d88
janarizf <janarizjoyf@gmail.com>
2022-11-23 00:41:56 +0800

    google signin

---
+++ src/components/login/google.auth.js
@@ -0,0 +1,24 @@
+const { OAuth2Client } = require('google-auth-library')
+
+/**
+ * @description Function to decode Google OAuth token
+ * @param token: string
+ * @returns ticket object
+ */
+export const getDecodedOAuthJwtGoogle = async token => {
+
+  const CLIENT_ID_GOOGLE = 'yourGoogleClientId'
+
+  try {
+    const client = new OAuth2Client(CLIENT_ID_GOOGLE)
+
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID_GOOGLE,
+    })
+
+    return ticket
+  } catch (error) {
+    return { status: 500, data: error }
+  }
+}
