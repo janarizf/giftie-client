18e07da81db5353914fa0598a062340af2fdca15
janarizf <janarizjoyf@gmail.com>
2023-08-20 12:26:08 +0800

    updated categories service

--- src/services/admin/categories.service.js
+++ src/services/admin/categories.service.js
@@ -2,20 +2,26 @@ import http from "../../http-common";
 
 class CategoriesDataService {
     async getAllListCategories() {
-        return await http.get("/admin/getAllListCategories")
+        return await http.get("/admin/category/list/getAllCategories")
             .then((response) => { return response })
             .catch((err) => {
                 console.log(err);
             });
     }
     async getAllThemesCategories() {
-        return await http.get("/admin/themes/getAllThemesCategories")
+        return await http.get("/admin/category/themes/getAllCategories")
             .then((response) => { return response })
             .catch((err) => {
                 console.log(err);
             });
     }
-    
+    async getAllItemsCategories() {
+        return await http.get("/admin/category/item/getAllCategories")
+            .then((response) => { return response })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
 }
 
 export default new CategoriesDataService();
