20690b439b8b53ab128fd5e227a02aec78302efd
janarizjoyf <janarizjoyf@gmail.com>
2023-08-25 02:46:38 +0800

    admin services

--- src/services/admin/adminlogin.service.js
+++ src/services/admin/adminlogin.service.js
@@ -2,11 +2,51 @@ import http from "../../http-common";
 
 class AdminLoginDataService {
     async adminLogin(data) {
-        return await http.get("/admin/login", data)
+        return await http.get("/admin/user/login", data)
             .then((response) => { return response })
             .catch((err) => {
                 console.log(err);
             });
+    }
+
+    async getAllAdminUsers() {
+        return await http.get("/admin/user/getAll")
+            .then((response) => { return response })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    async getAdminUserById(themeId) {
+        return await http
+          .get(`/admin/user/getById/${themeId}`)
+          .then((response) => {
+            return response;
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+      async createAdminUser(data) {
+        return await http.post("/admin/user/create", data)
+            .then((response) => { return response })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+    async updateAdminUser(id, data) {
+        return await http
+            .patch(`/admin/user/update/${id}`, data)
+            .then((response) => {
+                return response;
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    deleteAdminUser(id) {
+        return http.delete(`/admin/user/delete/${id}`);
     }
 }
 
--- src/services/admin/categories.service.js
+++ src/services/admin/categories.service.js
@@ -1,13 +1,48 @@
 import http from "../../http-common";
 
 class CategoriesDataService {
-    async getAllListCategories() {
+    async getAllListCategory() {
         return await http.get("/admin/category/list/getAllCategories")
             .then((response) => { return response })
             .catch((err) => {
                 console.log(err);
             });
     }
+
+    async getListCategoryById(themeId) {
+        return await http
+            .get(`/admin/category/list/getCategoryById/${themeId}`)
+            .then((response) => {
+                return response;
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    async createListCategory(data) {
+        return await http.post("/groups/category/list/create", data)
+            .then((response) => { return response })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    async updateListCategories(id, data) {
+        return await http
+            .patch(`/admin/category/list/update/${id}`, data)
+            .then((response) => {
+                return response;
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    deleteListCategory(id) {
+        return http.delete(`/admin/category/list/delete/${id}`);
+    }
+
     async getAllThemesCategories() {
         return await http.get("/admin/category/themes/getAllCategories")
             .then((response) => { return response })
@@ -15,12 +50,81 @@ class CategoriesDataService {
                 console.log(err);
             });
     }
+
+    async getThemeCategoryById(themeId) {
+        return await http
+            .get(`/admin/category/themes/getCategoryById/${themeId}`)
+            .then((response) => {
+                return response;
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    async createThemesCategory(data) {
+        return await http.post("/groups/category/themes/create", data)
+            .then((response) => { return response })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    async updateThemesCategory(id, data) {
+        return await http
+            .patch(`/admin/category/themes/update/${id}`, data)
+            .then((response) => {
+                return response;
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    deleteThemesCategory(id) {
+        return http.delete(`/admin/category/themes/delete/${id}`);
+    }
+
     async getAllItemsCategories() {
         return await http.get("/admin/category/item/getAllCategories")
             .then((response) => { return response })
             .catch((err) => {
                 console.log(err);
             });
+    }
+
+    async getItemCategoryById(themeId) {
+        return await http
+            .get(`/admin/category/item/getCategoryById/${themeId}`)
+            .then((response) => {
+                return response;
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    async createItemCategory(data) {
+        return await http.post("/groups/category/item/create", data)
+            .then((response) => { return response })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    async updateItemCategory(id, data) {
+        return await http
+            .patch(`/admin/category/item/update/${id}`, data)
+            .then((response) => {
+                return response;
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    deleteItemCategory(id) {
+        return http.delete(`/admin/category/item/delete/${id}`);
     }
 }
 
